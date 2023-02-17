985f4018c96ebe12cd2e3460790202fced00364a
Sergey Gvozdenko <exetto@gmail.com>
2023-02-17 22:07:03 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-const profileTitle = document.querySelector('.profile__title');
-const profileText = document.querySelector('.profile__text');
-const userName = document.querySelector('.popup__input_user_name');
-const userOccupation = document.querySelector('.popup__input_user_occupation');
-const popupForm = document.querySelector('.popup__form');
-const popup = document.querySelector('.popup');
-const profileEdit = document.querySelector('.profile__edit');
-const popupClose = document.querySelector('.popup__close');
+const profileTitle = document.querySelector(".profile__title");
+const profileText = document.querySelector(".profile__text");
+const userName = document.querySelector(".popup__input_user_name");
+const userOccupation = document.querySelector(".popup__input_user_occupation");
+const popupForm = document.querySelector(".popup__form");
+const popup = document.querySelector(".popup");
+const profileEdit = document.querySelector(".profile__edit");
+const popupClose = document.querySelector(".popup__close");
 
 // Заполнение полей формы данными из профиля
 function openPopup() {
@@ -28,5 +28,5 @@ const handleFormSubmit = function (event) {
 };
 
 popupForm.addEventListener("submit", handleFormSubmit);
-profileEdit.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+profileEdit.addEventListener("click", openPopup);
+popupClose.addEventListener("click", closePopup);
