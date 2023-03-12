22cfc30365cc1646dd97dcd8a45991d889a64c5b
Sergey Gvozdenko <exetto@gmail.com>
2023-03-12 21:30:05 +0300

    Отправка на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,32 +6,6 @@ const popupForm = document.querySelector(".popup__form");
 const popup = document.querySelector(".popup");
 const profileEdit = document.querySelector(".profile__edit");
 const popupClose = document.querySelector(".popup__close");
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // Заполнение полей формы данными из профиля
 function openPopup() {
